6e27618eb0831e64f3423b65320039a27ab0bd9b
grace <879983690@qq,com>
2018-09-14 11:53:18 +0800

    adding pserson.py,including class Person and class Manager

---
+++ person.py
@@ -0,0 +1,37 @@
+class Person:
+	def __init__(self, name, job, pay):
+		self.name = name
+		self.job = job
+		self.pay = pay
+	def lastName(self):
+		return self.name.split()[-1]
+	def giveRaise(self, percent):
+		self.pay = int(self.pay * (percent + 1))
+	def __str__(self):
+		return "Person---name:{},pay:{}".format(self.name, self.pay)
+
+
+class Manager(Person):
+	def __init__(self, name, pay):
+		Person.__init__(self, name, 'manager', pay)
+	def giveRaise(self, percent, bonus=.10):
+		Person.giveRaise(self, percent + bonus)
+
+
+if __name__ == "__main__":
+	a = Person('a', 'worker', 10)
+	b = Manager('b', 20)
+	print(a)
+	print(b)
+	b.giveRaise(0.1)
+	print(b)
+
+	print("Other testing……")
+	"""比较instance.__dict__,dir(instance)"""
+	print(a.__dict__.keys())
+	print(dir(a))
+	"""使用instance.__class__,class.__base__"""
+	print(a.__class__.__name__)    # Person
+	print(b.__class__.__name__)    # Manager
+	print(a.__class__.__base__)    # <class 'object'>
+	print(b.__class__.__base__)    # <class '__main__.Person'>
